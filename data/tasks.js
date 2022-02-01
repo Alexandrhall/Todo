90bf3fca0928e2a1a8c43386b75bb83f154837b8
Alexander <alexandrhall@gmail.com>
2022-02-01 19:53:36 +0100

    git

---
+++ data/tasks.js
@@ -0,0 +1,8 @@
+let date = ";";
+
+const tasks = [
+  { id: 1, created: date, discription: "", done: false },
+  { id: 1, created: date, discription: "", done: false },
+  { id: 1, created: date, discription: "", done: false },
+  { id: 1, created: date, discription: "", done: false },
+];
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
